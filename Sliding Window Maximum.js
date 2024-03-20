0f84106f35c4f3efbcf70a9ce33337462e75707a
dustics4 <konrad.babowicz21@gmail.com>
2024-03-20 23:47:16 +0000

    starting the question

---
+++ Sliding Window Maximum.js
@@ -0,0 +1,15 @@
+var maxSlidingWindow = function(nums, k) {
+    let tempA = [];
+    let tempS = 0;
+
+    //at every window check the max num within the window
+    //when you know the max num within the window e.g 3
+    //push into temporary sum or array
+    // make sure to remove the old one when repeating and adding to new
+    //repeat
+
+
+
+};
+
+maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3);
