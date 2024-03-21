583a977d657cdaf1ba7af36a4045bfe1e0a1fe7a
dustics4 <konrad.babowicz21@gmail.com>
2024-03-21 00:04:34 +0000

    resolution provided

--- Sliding Window Maximum.js
+++ Sliding Window Maximum.js
@@ -1,6 +1,6 @@
 var maxSlidingWindow = function(nums, k) {
-    let tempA = [];
-    let tempS = 0;
+    let queue = []; //stores the indices
+    let res = [];
 
     //at every window check the max num within the window
     //when you know the max num within the window e.g 3
@@ -8,7 +8,27 @@ var maxSlidingWindow = function(nums, k) {
     // make sure to remove the old one when repeating and adding to new
     //repeat
 
+    for(let i = 0; i < nums.length; i++){
+        //Pop the last num in queue while its less than the new num to be added.
+        while(queue.length > 0 && queue[queue.length - 1] < nums[i]){
+            queue.pop();
+        }
+        queue.push(nums[i]);
 
+        //if we have checked at least k elements, start pushing to resolution
+        if( i >= k - 1){
+            //Only push the largest num in queue which will always be the first num
+            res.push(queue[0]);
+            //move left part of the window over by 1
+            //index to be dequeued will be i - k + 1
+            // If queue[0] matches the num to be dequeued, dequeue it
+            const dequeueIndex = i - k + 1;
+            if(queue[0] === nums[i - k + 1]){
+                queue.shift();
+            }
+        }
+    }
+    console.log(res);
 
 };
 
