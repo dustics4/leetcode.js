a3afba0597757454b4e65db402da88e94ac4ede7
dustics4 <72154313+dustics4@users.noreply.github.com>
2024-04-12 22:18:47 +0100

    Add files via upload
    fixed array output to show within array

--- 3Sum.js
+++ 3Sum.js
@@ -10,7 +10,7 @@ function threeSum(nums, target){
         while(left < right){
             let sum = nums[i] + nums[left] + nums[right];
             if(sum === target){
-                result.push(nums[i], nums[left], nums[right]);
+                result.push([nums[i], nums[left], nums[right]]);
                 while(left < right && nums[left] === nums[left - 1]) left++;
                 while(left < right && nums[right] === nums[right -1]) right--;
                 left++;
